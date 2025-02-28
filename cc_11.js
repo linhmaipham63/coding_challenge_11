d1a990bb4d61beda8480c48c9920877a90c61158
Linh Pham <linhmaipham@usf.edu>
2025-02-27 22:20:46 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -31,3 +31,37 @@ console.log(book1.getDetails());
 book1.updateCopies(-1);
 console.log(book1.getDetails());
 
+
+// Task 2: Creating a Borrower Class
+
+// Create class Borrower
+class Borrower {
+    constructor(name, borrowerId, borrowedBooks) {
+        this.name = name; // Assign property name (string)
+        this.borrowerId = borrowerId; // Assign property borrowerId (number)
+        this.borrowedBooks = []; // Assign property borrowedBooks (array of borrowed book titles)
+    }
+
+    // Add method borrowBook(book) that adds a book title to borrowedBooks
+    borrowBook(book) {
+        this.borrowedBooks.push(book); 
+    }
+
+    // Add method returnBook(book) that removes the book from borrowedBooks
+    returnBook(book) {
+        const index = this.borrowedBooks.indexOf(book); 
+        if (index != -1) {
+            this.borrowedBooks.splice(index, 1); 
+        }
+    }
+
+}
+
+// Test Cases:
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+
